ae28f7500abd28da8d94624c39e20bd6dbb6c831
Profesor <Profesor@DESKTOP-0FE7I1O>
2021-04-14 19:28:54 +0100

    Commit arreglo texto

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -5,35 +5,11 @@ import java.io.InputStreamReader;
 public class HolaMundo {
 	
 	public static void main(String[] args) throws IOException {
-        // Variables para capturar las pulsaciones de teclado del usuario
-		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-        String valorEntrada;
-        //Esto es una prueba
-        
+      
         System.out.println("---------------------------------------------");
         System.out.println(" HOLA MUNDO");
         System.out.println("---------------------------------------------");
         
-        float ladoMenor=0;
-        do{
-        	// Solicitamos al usuario la longitud del lado menor del rectángulo
-        	System.out.println(" Indique la longitud del lado menor: ");
-        	valorEntrada = teclado.readLine();
-        	ladoMenor = Float.parseFloat(valorEntrada);
-        }while (ladoMenor<=0);
-        
-        float ladoMayor;
-        do {
-        // Solicitamos al usuario la longitud del lado mayor del rectángulo
-        System.out.println(" Indique la longitud del lado mayor: ");
-        valorEntrada = teclado.readLine();
-        ladoMayor = Float.parseFloat(valorEntrada);
-        }while (ladoMayor<=0);
-         
-        
-        // Calculamos y mostramos el área del rectángulo
-        float area = ladoMenor * ladoMayor;
-        System.out.println(" El área del rectángulo es: " + area);
         
 	}
 }
